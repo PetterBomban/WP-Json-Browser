c93398b35035e6af2fb5d1705610c4dcc2f01693
Petter Bomban <petterbomban@gmail.com>
2016-12-09 23:31:12 +0100

    Initial Commit

---
+++ Start-WpBrowser.ps1
@@ -0,0 +1,83 @@
+function Start-WpBrowser($Page = 1)
+{
+    while(1)
+    {
+        $Api = "https://SITE.COM/wp-json/wp/v2"
+        Get-WpPost -Api $Api -Page $Page |
+            Show-WpPostTitles -CurrPage $Page |
+                Show-WpPostContent
+    }
+}
+
+function Get-WpPost($Api, $Page)
+{
+    if ($Page) { $Uri = "$Api/posts?page=$Page" }
+    else { $Uri = "$Api/posts" }
+    $Result = Invoke-RestMethod -Uri $Uri -Method Get
+    $Collection = New-Object System.Collections.ArrayList
+    $Collection.Add($Result)
+    $Collection
+}
+
+function Show-WpPostTitles()
+{
+    param
+    (
+        [Parameter(
+            ValueFromPipeline = $True,
+            ValueFromPipelineByPropertyName = $True
+        )]
+        [Alias("title")]
+        $Titles,
+        $CurrPage = 1
+    )
+
+    ## Creating menu entries per post 
+    for ($i=1; $i -le $Titles.Count; $i++ )
+    {
+        Write-Host "$i) $($Titles[$i-1].title.rendered)"
+    }
+
+    Write-Host "11) Next page"
+    $Selected = Read-Host -Prompt "Select"
+    Clear-Host
+
+    if ($Selected -eq 11) { Start-WpBrowser -Page ($CurrPage + 1) }
+    $Titles[$Selected - 1]
+}
+
+function Show-WpPostContent()
+{
+    param
+    (
+        [Parameter(
+            ValueFromPipeline = $True
+        )]
+        $Post
+    )
+
+    Write-Host "$($Post.title.rendered)"
+    Write-Host "---------- `r`n"
+
+    ## Stripping tags and other unwanted stuff
+    $Content = $Post.content.rendered `
+        -replace "<[^>]+>", "" `
+        -replace "`r`n", "`r`n`r`n" `
+        -replace "&nbsp;", " " `
+        -replace "&laquo;", "<<" `
+        -replace "&raquo;", ">>"
+
+    Write-Host $Content
+
+    Write-Host "1) Open link"
+    Write-Host "2) Go back"
+    $Selected = Read-Host -Prompt "Select"
+
+    switch ($Selected)
+    {
+        "1" { Start-Process $Post.link; continue }
+        "2" { Clear-Host; continue }
+    }
+}
+
+Start-WpBrowser
---
+++ readme.md
@@ -0,0 +1,7 @@
+## WP-Json-Browser
+
+Small script to browse Wordpress sites with wp-json v2 enabled.
+
+Simply enter your API link near the top of the script, here:
+
+`$Api = "https://SITE.COM/wp-json/wp/v2"`
